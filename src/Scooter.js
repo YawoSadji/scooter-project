a6a42d1e541e7d0d64c326de565eac346f2348df
YawoSadji <yawo.sadji@gmx.com>
2024-09-13 14:58:44 -0400

    user class and scooter class completed

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,50 @@
 class Scooter {
   // scooter code here
-}
+  static nextSerial = 1;
+  constructor(station){
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+  rent(user){
+    if(this.charge > 20 && !this.isBroken){
+      this.station = null;
+      this.user = user;
+    }
+      else if(this.isBroken){
+        throw new Error('scooter needs repair.');
+      }
+      else if(this.charge < 20){
+        throw new Error('scooter needs to charge.');
+      }
+    }
+    dock(station){
+      if(this.user !== null){
+      this.station = station;
+      this.user = null;
+    }
+    }
+    async recharge() {
+        console.log('Starting charge');
+        while(this.charge < 100){
+        await new Promise(resolve => setTimeout(resolve, 2000)); // wait 2 seconds
+        this.charge += 10; 
+        if(this.charge > 100){
+            this.charge = 100;
+        }
+          console.log(`Charging...${this.charge}%`);
+        } 
+          console.log('Charge complete');
+    }
+    async requestRepair(){
+      console.log('Request repair');
+      await new Promise(resolve => setTimeout(resolve, 5000)); // wait 5 seconds
+      this.isBroken = false;
+      console.log('repair completed');
+    }
+
+  }
 
 module.exports = Scooter
